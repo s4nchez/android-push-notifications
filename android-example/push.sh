0f38e8d9e616897df329549efff1d92336a106ac
Ivan Sanchez <s4nchez@gmail.com>
2013-03-26 21:34:43 +0000

    Removed unused files

--- android-example/push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl --header "Authorization: key=AIzaSyA6-tx1UHIdB7o05kf-kIgsQtSTk0Rx8uw" \
-     --header Content-Type:"application/json" \
-     https://android.googleapis.com/gcm/send  \
-     -d '{"registration_ids":["APA91bHEKnWET4fCgy_28gUUMsxH35Zu8sy6FRxZblKTC9HUpGvh5n-lyuS65xdm4IXhmS9_S_Q58EF5r4_Z-rkqrFwqYexcRYUhBmI1Sp10suL2cjAlFIFVyk2m7pkho3HphPVVWpQDEoZY3WHRUUACmdHlbdoSQtKeOXahBoIA3OUBEvCbO2k"],"data":{"message":"Hi from Yazino!"}}'
-
